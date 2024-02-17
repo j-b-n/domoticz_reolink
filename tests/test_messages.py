ae94559447c07bcd544addc723a0279ff35a10e8
J-B-N <j-b-n-git@protonmail.com>
2024-02-17 19:18:03 +0100

    Fixed pytest test file.

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -3,7 +3,7 @@ sys.path.append('../')
 import reolink_utils
 
 def test_several_true():
-    with open("./messages/several_true.xml", "r") as file:
+    with open("./tests/messages/several_true.xml", "r") as file:
         #{'Motion': True, 'FaceDetect': False, 'PeopleDetect': True, 'VehicleDetect': False, 'DogCatDetect': False, 'MotionAlarm': True, 'Visitor': True, 'Any': True}
         data = file.read()
         parse_result = reolink_utils.reolink_parse_soap(data)
@@ -20,7 +20,7 @@ def test_several_true():
 
 def test_doorbell_on():
     #{'Motion': False, 'FaceDetect': False, 'PeopleDetect': False, 'VehicleDetect': False, 'DogCatDetect': False, 'MotionAlarm': False, 'Visitor': True, 'Any': True}
-    with open("./messages/Doorbell_on.xml", "r") as file:
+    with open("./tests/messages/Doorbell_on.xml", "r") as file:
         data = file.read()
         parse_result = reolink_utils.reolink_parse_soap(data)
 
@@ -35,10 +35,9 @@ def test_doorbell_on():
 
 def test_all_false():
     #{'Motion': False, 'FaceDetect': False, 'PeopleDetect': False, 'VehicleDetect': False, 'DogCatDetect': False, 'MotionAlarm': False, 'Visitor': False, 'Any': False}
-    with open("./messages/all_false.xml", "r") as file:
+    with open("./tests/messages/all_false.xml", "r") as file:
         data = file.read()
         parse_result = reolink_utils.reolink_parse_soap(data)
-        print(parse_result)
 
     assert parse_result["Motion"] == False
     assert parse_result["FaceDetect"] == False
@@ -51,7 +50,7 @@ def test_all_false():
 
 def test_motion_visitor_on():
     #{'Motion': True, 'FaceDetect': False, 'PeopleDetect': False, 'VehicleDetect': False, 'DogCatDetect': False, 'MotionAlarm': True, 'Visitor': True, 'Any': True}
-    with open("./messages/Motion_visitor_on.xml", "r") as file:
+    with open("./tests/messages/Motion_visitor_on.xml", "r") as file:
         data = file.read()
         parse_result = reolink_utils.reolink_parse_soap(data)
 
@@ -66,7 +65,7 @@ def test_motion_visitor_on():
 
 def test_person_on():
     #{'Motion': False, 'FaceDetect': False, 'PeopleDetect': True, 'VehicleDetect': False, 'DogCatDetect': False, 'MotionAlarm': False, 'Visitor': False, 'Any': True}
-    with open("./messages/Person_on.xml", "r") as file:
+    with open("./tests/messages/Person_on.xml", "r") as file:
         data = file.read()
         parse_result = reolink_utils.reolink_parse_soap(data)
 
